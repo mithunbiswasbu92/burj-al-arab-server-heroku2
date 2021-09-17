08a524e4d714ff86ab334b4b5a11cfbeeb2083c5
Mithun Biswas <mithunbiswasbu92@gmail.com>
2021-09-17 10:34:31 +0600

    burj al arab server second time done

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const admin = require("firebase-admin");
+require('dotenv').config()
+
+const port = 5000;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4svir.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+ 
+const app = express()
+app.use(cors());
+app.use(bodyParser.json());
+
+const serviceAccount = require("./configs/burj-al-arab-41761-firebase-adminsdk-a2pur-674752e11b.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const bookings = client.db("burjAlArab3").collection("bookings3");
+
+    app.post('/addBookings', (req, res) => {
+        const newBooking = req.body;
+        bookings.insertOne(newBooking)
+            .then(result => {
+                res.send(result.acknowledged == true)
+                console.log(result)
+            })
+    })
+
+    app.get('/bookings', (req, res) => {
+        const bearer = req.headers.authorization;
+        if (bearer && bearer.startsWith('Bearer ')) {
+            const idToken = bearer.split(' ')[1];
+            admin
+                .auth()
+                .verifyIdToken(idToken)
+                .then((decodedToken) => {
+                    const tokenEmail = decodedToken.email;
+                    const queryEmail = req.query.email; 
+                    if (tokenEmail == queryEmail) {
+                        bookings.find({ email: queryEmail })
+                            .toArray((err, documents) => {
+                                res.status(200).send(documents);
+                            })
+                    }
+                    else(
+                        res.status(401).send('Un-authorize Access')
+                    )
+                })
+                .catch((error) => {
+                    res.status(401).send('Un-authorize Access')
+                });
+        }
+        else(
+            res.status(401).send('Un-authorize Access')
+        )
+    })
+}); 
+
+app.listen(port)
